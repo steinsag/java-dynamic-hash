7dda5b23466e519d91aecc978f8b28d4586a82d7
Sebastian Stein <seb.kde@hpfsc.de>
2021-05-31 21:41:30 +0200

    add param for hash length and add unit tests

--- src/main/java/de/hpfsc/java/dynamichash/App.java
+++ src/main/java/de/hpfsc/java/dynamichash/App.java
@@ -7,17 +7,28 @@ import java.nio.charset.StandardCharsets;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(getHash("Hello World!"));
+        final String message = "Hello World!";
+        System.out.println(getHash(message, 64));
+        System.out.println(getHash(message, 30));
+        System.out.println(getHash(message, 20));
     }
 
-    static String getHash(final String message) {
+    static String getHash(final String message, final int lengthInCharacters) {
+        if (lengthInCharacters % 2 != 0 || lengthInCharacters <= 0) {
+            throw new IllegalArgumentException("lengthInCharacters must be an even positive number");
+        }
+
+        if (message == null || message.isBlank()) {
+            throw new IllegalArgumentException("message shall not be blank");
+        }
+
         final byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
 
         final SHAKEDigest digest = new SHAKEDigest(128);
 
-        final byte[] hashBytes = new byte[15];
+        final byte[] hashBytes = new byte[lengthInCharacters / 2];
         digest.update(messageBytes, 0, messageBytes.length);
-        digest.doFinal(hashBytes, 0, hashBytes.length);
+        digest.doOutput(hashBytes, 0, hashBytes.length);
 
         return Hex.toHexString(hashBytes);
     }
--- src/test/java/de/hpfsc/java/dynamichash/AppTest.java
+++ src/test/java/de/hpfsc/java/dynamichash/AppTest.java
@@ -2,12 +2,49 @@ package de.hpfsc.java.dynamichash;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class AppTest {
     @Test
-    public void checkHash() {
-        assertThat(App.getHash("Hello World!")).isEqualTo("aacfe6ebd3737d9f195c837c5281d3");
+    public void checkHashWithLength20() {
+        final String actualHash = App.getHash("Hello World!", 20);
+        final String expectedHash = "aacfe6ebd3737d9f195c";
+        assertThat(actualHash).isEqualTo(expectedHash);
+    }
+
+    @Test
+    public void checkHashWithLength30() {
+        final String actualHash = App.getHash("Hello World!", 30);
+        final String expectedHash = "aacfe6ebd3737d9f195c837c5281d3";
+        assertThat(actualHash).isEqualTo(expectedHash);
+    }
+
+    @Test
+    public void checkHashWithLength64() {
+        final String actualHash = App.getHash("Hello World!", 64);
+        final String expectedHash = "aacfe6ebd3737d9f195c837c5281d3f87646ecd7e43864e1a40456e40f264046";
+        assertThat(actualHash).isEqualTo(expectedHash);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 1, 3, 17})
+    public void checkInvalidArguments(int invalidLength) {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            App.getHash("some message", invalidLength);
+        });
+    }
+
+    @Test
+    public void checkInvalidMessages() {
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            App.getHash("", 16);
+        });
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            App.getHash(null, 16);
+        });
     }
 }
